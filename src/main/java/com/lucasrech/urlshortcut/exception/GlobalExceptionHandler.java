b508160ddcbbae9881eebdf5d62a2176fd2a3a53
lucas <lucasrech00@gmail.com>
2024-07-23 20:08:01 -0300

    fix: fix class handler name

--- src/main/java/com/lucasrech/urlshortcut/exception/GlobalExceptionHandler.java
+++ src/main/java/com/lucasrech/urlshortcut/exception/GlobalExceptionHandler.java
@@ -3,17 +3,18 @@ package com.lucasrech.urlshortcut.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class GlobalExceptionHandler {
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(UrlNotFoundedException.class)
+    @ExceptionHandler(UrlNotFoundedException.class)
     public ResponseEntity<ExceptionDTO> UrlNotFounded(UrlNotFoundedException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionDTO(exception.getMessage(), "404"));
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(UrlAlreadyExistsException.class)
+    @ExceptionHandler(UrlAlreadyExistsException.class)
     public ResponseEntity<ExceptionDTO> UrlAlreadyExists(UrlAlreadyExistsException exception) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(new ExceptionDTO(exception.getMessage(), "409"));
     }
